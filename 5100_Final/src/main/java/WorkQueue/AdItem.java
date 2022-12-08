c613ec0412099122bdd8fdeccc60d5b16ffbd20c
JinlangHe <he.jinl@northeastern.edu>
2022-12-07 21:08:38 -0500

    update

---
+++ 5100_Final/src/main/java/WorkQueue/AdItem.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WorkQueue;
+
+
+public class AdItem {
+    
+    private String adItemName;
+    private int adItemPrice;
+
+    public String getAdItemName() {
+        return adItemName;
+    }
+
+    public void setAdItemName(String adItemName) {
+        this.adItemName = adItemName;
+    }
+
+    public int getAdItemPrice() {
+        return adItemPrice;
+    }
+
+    public void setAdItemPrice(int adItemPrice) {
+        this.adItemPrice = adItemPrice;
+    }
+    
+    @Override
+    public String toString(){
+        return adItemName;
+    }
+    
+    
+}
