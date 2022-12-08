024ba85337b033b9a4cb6b452616d7f550b0f3fd
Yuemei chen <chen.yuem@northeastern.edu>
2022-12-08 14:39:21 -0500

    update

--- Person.java
+++ Person.java
@@ -10,13 +10,13 @@ public class Person {
     private String name;
 
 
-//    public String getName() {
-//        return name;
-//    }
+    public String getName() {
+        return name;
+    }
 
-//    public void setName(String name) {
-//        this.name = name;
-//    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
