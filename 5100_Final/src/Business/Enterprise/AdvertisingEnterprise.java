ac6c1f2040eabf9bc0b1780c071b4199512b0c8a
Cheng Ming <cheng.ming2@northeastern.edu>
2022-12-09 21:40:13 -0500

    configure enterprise

---
+++ 5100_Final/src/Business/Enterprise/AdvertisingEnterprise.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Enterprise;
+
+/**
+ *
+ * @author JINLANG
+ */
+public class AdvertisingEnterprise extends Enterprise{
+
+    public AdvertisingEnterprise(String name) {
+        super(name, EnterpriseType.AdvertisingEnterprise);
+    }
+    
+}
