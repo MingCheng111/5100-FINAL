21cdbad16c3c1c37362d73c834ba2062846bb9e2
JinlangHe <he.jinl@northeastern.edu>
2022-12-10 22:55:29 -0500

    Role
    update

---
+++ 5100_Final/src/main/java/Enterprise/Population.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Enterprise;
+
+/**
+ *
+ * @author JINLANG
+ */
+public class Population extends Enterprise{
+
+    public Population(String name) {
+        super(name, Enterprise.EnterpriseType.Population);
+    }
+    
+}
